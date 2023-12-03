c4b9aa67a30ce4d41b243bae1a09eb4daedbdfa5
USER <sithiraroneth@gmail.com>
2023-12-04 01:10:30 +0530

    ..

--- Array/src/Main.java
+++ Array/src/Main.java
@@ -4,8 +4,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         String[] ar = {"s", "d", "f", "g", "s", "d"};
